2274cdacd37c64cf80ede7838930a7ad08a3f231
itc-s23001 <s23001@std.it-college.ac.jp>
2024-02-08 07:54:15 +0000

    9fin

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,6 @@
-import { getPostBySlug } from 'lib/api'
+import { getPostBySlug, getAllSlugs } from 'lib/api'
 import extractText from 'lib/extract-text'
+import { prevNextPost } from 'lib/prev-next-post'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
@@ -11,15 +12,21 @@ import {
   TwoColumnMain,
   TwoColumnSidebar
 } from 'components/two-column'
+import Pagination from 'components/pagination'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Schedule = ({
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+const Post = ({
   title,
   publish,
   content,
   eyecatch,
   categories,
-  description
+  description,
+  prevPost,
+  nextPost
 }) => {
   return (
     <Container>
@@ -32,9 +39,9 @@ const Schedule = ({
       />
       <article>
         <PostHeader title={title} subtitle='Blog Article' publish={publish} />
-
         <figure>
           <Image
+            key={eyecatch.url}
             src={eyecatch.url}
             alt=''
             layout='responsive'
@@ -42,9 +49,10 @@ const Schedule = ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
-
         <TwoColumn>
           <TwoColumnMain>
             <PostBody>
@@ -55,26 +63,51 @@ const Schedule = ({
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
       </article>
     </Container>
   )
 }
-
-const getStaticProps = async () => {
-  const slug = 'schedule'
+const getStaticPaths = async () => {
+  const allslugs = await getAllSlugs()
+  return {
+    paths: allslugs.map(({ slug }) => `/blog/${slug}`),
+    fallback: false
+  }
+}
+const getStaticProps = async ({ params }) => {
+  const { slug } = params
   const post = await getPostBySlug(slug)
+  if (!post) {
+    return {
+      notFound: true
+    }
+  }
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
+
+  const allSlugs = await getAllSlugs()
+  const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
-      description: description
+      description: description,
+      prevPost: prevPost,
+      nextPost: nextPost
     }
   }
 }
-
-export { getStaticProps }
-export default Schedule
+export default Post
+export { getStaticProps, getStaticPaths }
